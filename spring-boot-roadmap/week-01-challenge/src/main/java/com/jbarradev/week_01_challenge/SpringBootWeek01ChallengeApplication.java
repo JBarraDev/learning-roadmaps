d94b7dfafd0716bce7c602a5b0d67f5b298a67b4
Javier Barra <revistaspgo@gmail.com>
2026-01-27 16:02:41 +0100

    chore: initialize project structure

---
+++ spring-boot-roadmap/week-01-challenge/src/main/java/com/jbarradev/week_01_challenge/SpringBootWeek01ChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.jbarradev.week_01_challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWeek01ChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWeek01ChallengeApplication.class, args);
+	}
+
+}
