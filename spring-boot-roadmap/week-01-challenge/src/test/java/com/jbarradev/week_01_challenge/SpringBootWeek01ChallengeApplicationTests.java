f6e64e8bd7f540344653c2aca13cb75c505adf76
Javier Barra <revistaspgo@gmail.com>
2026-01-27 22:44:56 +0100

    chore: add project configuration

---
+++ spring-boot-roadmap/week-01-challenge/src/test/java/com/jbarradev/week_01_challenge/SpringBootWeek01ChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jbarradev.week_01_challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWeek01ChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
