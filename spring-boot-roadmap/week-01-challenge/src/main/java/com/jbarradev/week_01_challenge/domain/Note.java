15a8991b9fb11ebaa65b3b057f644f868124bcb6
Javier Barra <revistaspgo@gmail.com>
2026-01-27 16:04:52 +0100

    feat: add Note domain

---
+++ spring-boot-roadmap/week-01-challenge/src/main/java/com/jbarradev/week_01_challenge/domain/Note.java
@@ -0,0 +1,17 @@
+package com.jbarradev.week_01_challenge.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Note {
+
+    private Long id;
+    private String title;
+    private String content;
+    private LocalDateTime createdAy;
+}
