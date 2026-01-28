e0d055423e5706b8c1b4890cdae627db943c0910
Javier Barra <revistaspgo@gmail.com>
2026-01-28 10:26:52 +0100

    chore: initialize project with structure and init config

---
+++ spring-boot-roadmap/week-02/src/test/java/com/jbarradev/week02/Week02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jbarradev.week02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
