1a47abe678471e4f08d2558635a9592215d0914d
Javier Barra <revistaspgo@gmail.com>
2026-01-29 11:06:50 +0100

    chore: remove unused import

--- spring-boot-roadmap/week-02/src/main/java/com/jbarradev/week02/dto/TaskRequestDTO.java
+++ spring-boot-roadmap/week-02/src/main/java/com/jbarradev/week02/dto/TaskRequestDTO.java
@@ -2,7 +2,6 @@ package com.jbarradev.week02.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
-import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
