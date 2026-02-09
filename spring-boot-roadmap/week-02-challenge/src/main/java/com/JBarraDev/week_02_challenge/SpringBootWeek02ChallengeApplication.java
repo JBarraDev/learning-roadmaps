9d21a69f9bb1b7a7f1c639a6bbdedd18e2315dfe
Javier Barra <revistaspgo@gmail.com>
2026-02-09 11:27:33 +0100

    chore: initialize project with structure and init config

---
+++ spring-boot-roadmap/week-02-challenge/src/main/java/com/JBarraDev/week_02_challenge/SpringBootWeek02ChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.JBarraDev.week_02_challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWeek02ChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWeek02ChallengeApplication.class, args);
+	}
+
+}
---
+++ spring-boot-roadmap/week-02-challenge/src/test/java/com/JBarraDev/week_02_challenge/SpringBootWeek02ChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JBarraDev.week_02_challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWeek02ChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
